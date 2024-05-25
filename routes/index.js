48ff00975cd7b565d9bfe6be8e9217eb2e066d1d
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2024-05-25 21:22:56 +0500

    Updated Code

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 const csurf = require('csurf');
+const { Sequelize } = require('sequelize');
+const SeoModel = require('../Models/Seo');
+
 
 var router = express.Router();
 
@@ -8,68 +11,74 @@ const csrfProtection = csurf({ cookie: true });
 
 
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about', function(req, res, next) {
+router.get('/about', function (req, res, next) {
   res.render('about', { title: 'Express' });
 });
 
-router.get('/blogs', function(req, res, next) {
+router.get('/blogs', function (req, res, next) {
   res.render('blogs', { title: 'Express' });
 });
 
-router.get('/contact', function(req, res, next) {
+router.get('/contact', function (req, res, next) {
   res.render('contact', { title: 'Express' });
 });
 
-router.get('/farmer-details', function(req, res, next) {
+router.get('/farmer-details', function (req, res, next) {
   res.render('farmer-details', { title: 'Express' });
 });
 
-router.get('/farmer', function(req, res, next) {
+router.get('/farmer', function (req, res, next) {
   res.render('farmer', { title: 'Express' });
 });
 
-router.get('/project-detail', function(req, res, next) {
+router.get('/project-detail', function (req, res, next) {
   res.render('project-detail', { title: 'Express' });
 });
 
-router.get('/project', function(req, res, next) {
+router.get('/project', function (req, res, next) {
   res.render('project', { title: 'Express' });
 });
 
-router.get('/service', function(req, res, next) {
+router.get('/service', function (req, res, next) {
   res.render('service', { title: 'Express' });
 });
 
-router.get('/service-detail', function(req, res, next) {
+router.get('/service-detail', function (req, res, next) {
   res.render('service-detail', { title: 'Express' });
 });
 
-router.get('/shop', function(req, res, next) {
+router.get('/shop', function (req, res, next) {
   res.render('shop', { title: 'Express' });
 });
 
-router.get('/shop-single', function(req, res, next) {
+router.get('/shop-single', function (req, res, next) {
   res.render('shop-single', { title: 'Express' });
 });
 
 
-router.get('/admin', function(req, res, next) {
-  res.render('admin/index', { title: 'Express' ,layout:'admin'});
+router.get('/admin', function (req, res, next) {
+  res.render('admin/index', { title: 'Express', layout: 'admin' });
 });
 
 
-router.get('/admin-blog',csrfProtection, function(req, res, next) {
-  res.render('admin/Blogs/index', { title: 'Express' ,layout:'admin'});
+router.get('/admin-blog', csrfProtection, async function (req, res, next) {
+
+  const BlogSeo = await SeoModel.findOne({ where: { uniqueId: 'blog-page' } });
+  console.log(BlogSeo.dataValues)
+
+  const blogsMeta = BlogSeo.dataValues;
+
+  res.render('admin/Blogs/index', { title: 'Express',blogsMeta, layout: 'admin' });
 });
 
 
 
 
-router.get('*', function(req, res, next) {
+router.get('*', function (req, res, next) {
   res.render('404', { title: 'Express' });
 });
 
